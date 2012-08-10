da1208a0dd4f0f4e9b4b77b0b4a8034ec6373334
Rico P <MailToRico@googlemail.com>
2012-08-10 13:24:25 +0200

    made lib smaler

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 cpp src/compile.js -P -I include > compiler.js
 
-rm lib.js
-echo "#ifndef COMPILER_JS" >> lib.js 
-echo "#define COMPILER_JS" >> lib.js
-cat compiler.js >> lib.js
-echo "#endif" >> lib.js 
