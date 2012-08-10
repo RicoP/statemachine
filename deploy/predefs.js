6c2fc261fce24ca3f12daea0c6584fc1cecc567c
Rico P <MailToRico@googlemail.com>
2012-08-10 18:39:25 +0200

    tweaked ui

--- deploy/predefs.js
+++ deploy/predefs.js
@@ -1,28 +1,30 @@
-
+function Print(callback /*, arg1, arg2, arg3, ... */) {
+	var args = Array.prototype.slice.call(arguments);
+	args.shift(); 
+	var message = args.join(" "); 
+	console.log(message); 
+	$("#output").html(message); 
+	if(callback) callback(); 
+}
 
 function Choose(callback, message, a, b) {
-	console.log("Choose:", message, a, b);
+	Print(null, "Choose:", message, a, b);
 	var answer = ((confirm((("You want a " + a + "?"))) ? a : b));
 	callback(null, answer);
 }
 
 function Say(callback, message) {
-	console.log("Say:", message);
+	Print(null, "Say:", message);
 	window.setTimeout(callback, 2000);
 }
 
 function GiveItem(callback, item) {
-	console.log("GiveItem:", item);
+	Print(null, "GiveItem:", item);
 	window.setTimeout(callback, 2000);
 }
 
 function Sleep(callback, time) {
 	window.setTimeout(callback, time); 
-}
-
-function Print(callback, message) {
-	console.log(message); 
-	callback(); 
 }
 
 function OnTrigger(callback, func) {
